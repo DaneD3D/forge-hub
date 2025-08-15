cd746e6860e4048e9daae8e18698d09245a2949e
Dane_D <dane.danovskis@fsastore.com>
2025-08-15 14:02:18 -0400

    added automatic docker opening

---
+++ start-backend.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check if Docker is running
+docker info >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Docker daemon not running. Please start Docker Desktop and try again."
+  exit 1
+fi
+
+# Start the SAM API
+sam local start-api --template lambda.yaml --env-vars dev-env.json
